e68c6acf873971eda0948bc989221bfbf288a401
Ahdyou <125517508+Ahdyou@users.noreply.github.com>
2023-05-02 00:16:55 +0100

    Update 0-print_listint.c

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -3,20 +3,18 @@
 
 /**
  * print_ listintt
- * @h:linked llist 
+ * @h: linked llist 
  *
  * Return:the  number of nodes
  */
 size_t print_listint(const listint_t *h)
 {
 	size_t num = 0;
-
 	while (h)
 	{
 		printf("%d\n", h->n);
 		h = h->next;
 		num++;
 	}
-
 	return (num);
 }
